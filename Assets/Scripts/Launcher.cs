79c5538f4d542a5631d824753f9bafa64ac02016
Benjamin Blázsovics <ahim13@gmail.com>
2017-09-02 22:52:15 +0200

    LobbyScene with Lobby UI created (started)

---
+++ Assets/Scripts/Launcher.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon;
+
+namespace NostrumGames
+{
+    public class Launcher : PunBehaviour
+    {
+
+        [SerializeField]
+        private PhotonLogLevel _logLevel = PhotonLogLevel.Informational;
+        [SerializeField]
+        private byte _maxPlayersPerRoom = 4;
+
+        private string _gameVersion = "1";
+
+
+        void Awake()
+        {
+            PhotonNetwork.autoJoinLobby = false;
+            PhotonNetwork.automaticallySyncScene = true;
+            PhotonNetwork.logLevel = _logLevel;
+        }
+
+        void Start()
+        {
+            Connect();
+        }
+
+
+        private void Connect()
+        {
+            if (PhotonNetwork.connected)
+            {
+                PhotonNetwork.JoinRandomRoom();
+                Debug.Log("RANDOM ROOM");
+            }
+            else
+            {
+                PhotonNetwork.ConnectUsingSettings(_gameVersion);
+            }
+        }
+
+        public override void OnConnectedToMaster()
+        {
+            Debug.Log("Onconnected was called");
+
+            Debug.Log("Rooms: ");
+
+            PhotonNetwork.JoinRandomRoom();
+        }
+
+        public override void OnJoinedLobby()
+        {
+            Debug.Log("Joined Lobby");
+        }
+
+        public override void OnDisconnectedFromPhoton()
+        {
+            Debug.LogWarning("Disconnected from Photon");
+        }
+
+        public override void OnPhotonRandomJoinFailed(object[] codeAndMsg)
+        {
+            Debug.Log("No random room available, so we create one.");
+
+            PhotonNetwork.CreateRoom(null, new RoomOptions() { MaxPlayers = _maxPlayersPerRoom }, null);
+        }
+
+        public override void OnJoinedRoom()
+        {
+            Debug.Log("Now this client is in a room.");
+            Debug.Log("InLobby: " + PhotonNetwork.insideLobby);
+        }
+    }
+}
---
+++ Assets/Scripts/Models/Network/Room.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace NostrumGames
+{
+    public class Room : MonoBehaviour
+    {
+
+        [SerializeField]
+        private Text _roomNameText;
+
+        [SerializeField]
+        private Image _isRoomPrivateImage;
+
+
+        [SerializeField]
+        private Text _roomSizeText;
+
+
+        public void SetRoomInfo(string roomNameText, bool isRoomPrivate, string roomSizeText)
+        {
+            _roomNameText.text = roomNameText;
+            _isRoomPrivateImage.enabled = isRoomPrivate;
+            _roomSizeText.text = roomSizeText;
+        }
+
+        public string GetRoomSizeAndPlayersAsString(int maxPlayers, int currentPlayers)
+        {
+            return maxPlayers.ToString() + "/" + currentPlayers.ToString();
+        }
+
+
+    }
+}
