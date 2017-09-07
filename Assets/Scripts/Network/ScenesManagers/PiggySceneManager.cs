bc716ee4e419e8335734603ec80c79fd53ad840a
Benjamin Blázsovics <ahim13@gmail.com>
2017-09-07 14:55:25 +0200

    RandomSeed created

---
+++ Assets/Scripts/Network/ScenesManagers/PiggySceneManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NostrumGames
+{
+
+    public class PiggySceneManager : MonoBehaviour
+    {
+        public int seed;
+
+        void Awake()
+        {
+            SetRandomSeed();
+        }
+
+        public void SetRandomSeed()
+        {
+            // int seed;
+            seed = SeedGenerator.GenerateSeed();
+            Random.InitState(seed);
+        }
+
+    }
+}
---
+++ Assets/Scripts/Tools/RandomSeed.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSeed : MonoBehaviour
+{
+    public int Seed;
+
+    void Awake()
+    {
+        Random.InitState(Seed);
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) Time.timeScale = 0;
+        if (Input.GetMouseButtonDown(1)) Time.timeScale = 1;
+    }
+}
