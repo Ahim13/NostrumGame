c48e5498e3bc63bcce530db423f5db33020c3b58
Blázsovics Benjamin <ahim13@gmail.com>
2017-10-15 21:40:20 +0200

    DeatMenu animation added

---
+++ Assets/Scripts/Effects/SlideInOutUI.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class SlideInOutUI : MonoBehaviour
+{
+
+    public float ReferencPoint;
+    public float Duration;
+    public Ease Ease;
+
+    private RectTransform _rect;
+
+    #region Unity Methods
+
+    void OnEnable()
+    {
+        _rect = GetComponent<RectTransform>();
+
+        _rect.DOAnchorPosX(ReferencPoint, Duration, false).SetEase(Ease).From();
+    }
+
+    #endregion
+}
