6835b854758d0bff089f1d0db315ec6908e98790
Blázsovics Benjamin <ahim13@gmail.com>
2017-10-15 14:55:47 +0200

    DeathText animation created

---
+++ Assets/Scripts/Effects/TextReveal.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+namespace NostrumGames
+{
+    [RequireComponent(typeof(TextMeshProUGUI))]
+    public class TextReveal : MonoBehaviour
+    {
+
+        [SerializeField]
+        private float _speed;
+
+        private TextMeshProUGUI _text;
+
+        #region Unity Methods
+
+        void Awake()
+        {
+            _text = GetComponent<TextMeshProUGUI>();
+        }
+
+        void OnEnable()
+        {
+            StartCoroutine(RevealCharacters());
+        }
+
+        void OnDisable()
+        {
+            StopCoroutine(RevealCharacters());
+        }
+
+        #endregion
+
+
+        IEnumerator RevealCharacters()
+        {
+
+            var totalVisibleCharacter = _text.textInfo.characterCount;
+            var counter = 0;
+
+            while (counter <= totalVisibleCharacter)
+            {
+                var visibleCount = counter % (totalVisibleCharacter + 1);
+
+                _text.maxVisibleCharacters = visibleCount;
+
+                counter += 1;
+
+                yield return new WaitForSeconds(_speed);
+            }
+
+        }
+    }
+}
